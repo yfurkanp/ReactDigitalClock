d16164c2ac22de1bba4b2dd15902eda76b6d618b
Yasir Furkan <71411916+yfurkanp@users.noreply.github.com>
2022-11-12 19:12:42 +0300

    Add files via upload

---
+++ 10-digitalClock.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import "../assets/css/10-digitalClock.css"
+import moment from "moment";
+
+
+
+const DigitalClock = (props) => {
+
+    const dateTime=moment();
+    
+    const dateStr = dateTime.format("LL");
+    const timeStr = dateTime.format("HH:mm");
+    const dayStr = dateTime.format("dddd");
+    const hour=dateTime.format("HH");
+    let message="";
+
+    if(hour>=6 && hour<11){
+        message="Morning";
+    }else if(hour>=11 && hour<17){
+        message="Afternoon";
+    }else if(hour>=17 && hour<21){
+        message="Evening";
+    }else{
+        message="Night";
+    }
+
+    const stil={
+        backgroundColor:props.bgColor,
+        color:props.color
+    }
+
+  return (
+    <div className="clock-container" style={stil}>
+        <div className="time">{timeStr}</div>
+        <div>
+            <div className="date">{dateStr}</div>
+            <div className="day">{dayStr} {message}</div>
+        </div>
+    </div>
+  )
+}
+
+export default DigitalClock
